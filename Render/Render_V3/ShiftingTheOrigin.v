e795329fb48878bba4c8b452cd3ccb78e0b86cfc
Nemo7743 <legoabc0@gmail.com>
2025-06-18 20:33:32 +0800

    Update ShiftingTheOrigin.v

--- Render/Render_V3/ShiftingTheOrigin.v
+++ Render/Render_V3/ShiftingTheOrigin.v
@@ -28,20 +28,20 @@ module ShiftingTheOrigin(
     //null
     ///////////////////////////////////////////////////shifting
     assign
-        vtx1_X = vtx1_X_scaled + 20'h50000,//320
-        vtx1_Y = vtx1_Y_scaled + 20'h3c000,//240
+        vtx1_X = vtx1_X_scaled + 21'h050000,//320
+        vtx1_Y = vtx1_Y_scaled + 21'h03c000,//240
         vtx1_Z = vtx1_Z_scaled,
         
-        vtx2_X = vtx2_X_scaled + 20'h50000,//320
-        vtx2_Y = vtx2_Y_scaled + 20'h3c000,//240
+        vtx2_X = vtx2_X_scaled + 21'h050000,//320
+        vtx2_Y = vtx2_Y_scaled + 21'h03c000,//240
         vtx2_Z = vtx2_Z_scaled,
         
-        vtx3_X = vtx3_X_scaled + 20'h50000,//320
-        vtx3_Y = vtx3_Y_scaled + 20'h3c000,//240
+        vtx3_X = vtx3_X_scaled + 21'h050000,//320
+        vtx3_Y = vtx3_Y_scaled + 21'h03c000,//240
         vtx3_Z = vtx3_Z_scaled,
         
-        vtx4_X = vtx4_X_scaled + 20'h50000,//320
-        vtx4_Y = vtx4_Y_scaled + 20'h3c000,//240
+        vtx4_X = vtx4_X_scaled + 21'h050000,//320
+        vtx4_Y = vtx4_Y_scaled + 21'h03c000,//240
         vtx4_Z = vtx4_Z_scaled;
     ///////////////////////////////////////////////////shifting end
 endmodule
