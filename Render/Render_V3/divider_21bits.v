d43a99cfc1fceb3078585a191b13ffb214a50911
owltorys <c931012c@gmail.com>
2025-06-19 16:18:53 +0800

    Update divider_21bits.v

--- Render/Render_V3/divider_21bits.v
+++ Render/Render_V3/divider_21bits.v
@@ -5,17 +5,16 @@ module divider_21bits(clk, open, dividend, divisor, finish, quotient);
     input [20:0] dividend, divisor;
     output finish;
     output [20:0] quotient;
-    wire [2:0] quotient_front;
-    wire [23:0] remainder;
+    wire [18:0] quotient_useless;
 
     ip_divider_21bits core(
         .aclk(clk),
         .s_axis_dividend_tvalid(open),
-        .s_axis_dividend_tdata(dividend),
+        .s_axis_dividend_tdata({{3{dividend[20]}}, dividend}),
         .s_axis_divisor_tvalid(open),
-        .s_axis_divisor_tdata(divisor),
+        .s_axis_divisor_tdata({{3{divisor[20]}}, divisor}),
         .m_axis_dout_tvalid(finish),
-        .m_axis_dout_tdata({quotient_front, quotient, remainder})
+        .m_axis_dout_tdata({quotient_useless[18:1], quotient[20:18], quotient_useless[0], quotient[17:0]})
     );
 
 endmodule
