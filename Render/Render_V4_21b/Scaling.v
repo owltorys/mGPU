e3fcfcf01f1e4156f13a0cd59567cad90c1b2154
owltorys <c931012c@gmail.com>
2025-06-19 21:40:21 +0800

    Update Scaling.v

--- Render/Render_V4_21b/Scaling.v
+++ Render/Render_V4_21b/Scaling.v
@@ -34,17 +34,17 @@ module Scaling(
         vtx4_X_raw_buffer, vtx4_Y_raw_buffer;
     
     assign
-        vtx1_X_raw_buffer = 21'h50000 * vtx1_X_raw,
-        vtx1_Y_raw_buffer = 21'h3c000 * vtx1_Y_raw,
+        vtx1_X_raw_buffer = $signed(21'h50000) * vtx1_X_raw,
+        vtx1_Y_raw_buffer = $signed(21'h3c000) * vtx1_Y_raw,
         
-        vtx2_X_raw_buffer = 21'h50000 * vtx2_X_raw,
-        vtx2_Y_raw_buffer = 21'h3c000 * vtx2_Y_raw,
+        vtx2_X_raw_buffer = $signed(21'h50000) * vtx2_X_raw,
+        vtx2_Y_raw_buffer = $signed(21'h3c000) * vtx2_Y_raw,
         
-        vtx3_X_raw_buffer = 21'h50000 * vtx3_X_raw,
-        vtx3_Y_raw_buffer = 21'h3c000 * vtx3_Y_raw,
+        vtx3_X_raw_buffer = $signed(21'h50000) * vtx3_X_raw,
+        vtx3_Y_raw_buffer = $signed(21'h3c000) * vtx3_Y_raw,
         
-        vtx4_X_raw_buffer = 21'h50000 * vtx4_X_raw,
-        vtx4_Y_raw_buffer = 21'h3c000 * vtx4_Y_raw;
+        vtx4_X_raw_buffer = $signed(21'h50000) * vtx4_X_raw,
+        vtx4_Y_raw_buffer = $signed(21'h3c000) * vtx4_Y_raw;
 
     assign
         vtx1_X_scaled = vtx1_X_raw_buffer >>> 18,
