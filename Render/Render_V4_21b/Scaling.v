dae87efd19c347269c7f1804323da1fb22a574b5
owltorys <c931012c@gmail.com>
2025-06-19 20:55:58 +0800

    Update Scaling.v

--- Render/Render_V4_21b/Scaling.v
+++ Render/Render_V4_21b/Scaling.v
@@ -34,33 +34,33 @@ module Scaling(
         vtx4_X_raw_buffer, vtx4_Y_raw_buffer;
     
     assign
-        vtx1_X_raw_buffer = 21'h2800 * vtx1_X_raw,
-        vtx1_Y_raw_buffer = 21'h1e00 * vtx1_Y_raw,
+        vtx1_X_raw_buffer = 21'h50000 * vtx1_X_raw,
+        vtx1_Y_raw_buffer = 21'h3c000 * vtx1_Y_raw,
         
-        vtx2_X_raw_buffer = 21'h2800 * vtx2_X_raw,
-        vtx2_Y_raw_buffer = 21'h1e00 * vtx2_Y_raw,
+        vtx2_X_raw_buffer = 21'h50000 * vtx2_X_raw,
+        vtx2_Y_raw_buffer = 21'h3c000 * vtx2_Y_raw,
         
-        vtx3_X_raw_buffer = 21'h2800 * vtx3_X_raw,
-        vtx3_Y_raw_buffer = 21'h1e00 * vtx3_Y_raw,
+        vtx3_X_raw_buffer = 21'h50000 * vtx3_X_raw,
+        vtx3_Y_raw_buffer = 21'h3c000 * vtx3_Y_raw,
         
-        vtx4_X_raw_buffer = 21'h2800 * vtx4_X_raw,
-        vtx4_Y_raw_buffer = 21'h1e00 * vtx4_Y_raw;
+        vtx4_X_raw_buffer = 21'h50000 * vtx4_X_raw,
+        vtx4_Y_raw_buffer = 21'h3c000 * vtx4_Y_raw;
 
     assign
-        vtx1_X_scaled = vtx1_X_raw_buffer >>> 10,
-        vtx1_Y_scaled = vtx1_Y_raw_buffer >>> 10,
+        vtx1_X_scaled = vtx1_X_raw_buffer >>> 18,
+        vtx1_Y_scaled = vtx1_Y_raw_buffer >>> 18,
         vtx1_Z_scaled = vtx1_Z_raw,
 
-        vtx2_X_scaled = vtx2_X_raw_buffer >>> 10,
-        vtx2_Y_scaled = vtx2_Y_raw_buffer >>> 10,
+        vtx2_X_scaled = vtx2_X_raw_buffer >>> 18,
+        vtx2_Y_scaled = vtx2_Y_raw_buffer >>> 18,
         vtx2_Z_scaled = vtx2_Z_raw,
 
-        vtx3_X_scaled = vtx3_X_raw_buffer >>> 10,
-        vtx3_Y_scaled = vtx3_Y_raw_buffer >>> 10,
+        vtx3_X_scaled = vtx3_X_raw_buffer >>> 18,
+        vtx3_Y_scaled = vtx3_Y_raw_buffer >>> 18,
         vtx3_Z_scaled = vtx3_Z_raw,
 
-        vtx4_X_scaled = vtx4_X_raw_buffer >>> 10,
-        vtx4_Y_scaled = vtx4_Y_raw_buffer >>> 10,
+        vtx4_X_scaled = vtx4_X_raw_buffer >>> 18,
+        vtx4_Y_scaled = vtx4_Y_raw_buffer >>> 18,
         vtx4_Z_scaled = vtx4_Z_raw;
     /*
     assign
